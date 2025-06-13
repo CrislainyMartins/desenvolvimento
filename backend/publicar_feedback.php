acc20f8af496d20d60e665df655e2ba15d95736c
CrislainyMartins <crislainy6339@gmail.com>
2025-06-13 14:05:48 -0300

    v2

---
+++ backend/publicar_feedback.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'] ?? 'Anônimo'; // Nome opcional
+    $avaliacao = $_POST['avaliacao'] ?? ''; //variavel da coluna avaliacao
+    $sugestao = $_POST['sugestao'] ?? ''; //variavel da coluna sugestao
+
+    // Validar a mensagem
+    if (trim($sugestao) === '') {
+        die("A mensagem não pode estar vazia.");
+    }
+
+    // Inserir no banco de dados
+    $sql = "INSERT INTO feedbacks (	nome, avaliacao, sugestao) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $nome, $avaliacao,  $sugestao);
+
+    if ($stmt->execute()) {
+        echo "Feedback enviado com sucesso!";
+    } else {
+        echo "Erro ao enviar o feedback: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
