53435590fa7bf238d9041edb040fc25799f3fe61
CrislainyMartins <crislainy6339@gmail.com>
2025-06-23 16:11:39 -0300

    v9

--- backend/gerenciar_feedback.php
+++ backend/gerenciar_feedback.php
@@ -46,7 +46,7 @@
                 </button></a>
 
             <!--botão de sair admin-->
-            <a href="restrito/logout.php">
+            <a href="../restrito/logout.php">
                 <button>
                     <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024">
                         <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path>
@@ -69,7 +69,7 @@
         function aprovarDepoimento(id) {
             if (confirm("Tem certeza de que deseja aprovar este feedback?")) {
                 // Envia uma requisição POST para o servidor
-                fetch('../desenvolvimento/backend/acoes_depoimentos.php', {
+                fetch('../desenvolvimento/backend/acoes_feedback.php', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded',
@@ -84,16 +84,16 @@
                         location.reload(); // Recarrega a página para atualizar a lista
                     })
                     .catch(error => {
-                        alert("Erro ao aprovar o depoimento: " + error);
+                        alert("Erro ao aprovar o feedback: " + error);
                     });
             }
         }
 
-        // Função para excluir depoimento
+        // Função para excluir feedback
         function excluirDepoimento(id) {
             if (confirm("Tem certeza de que deseja excluir este depoimento?")) {
                 // Envia uma requisição POST para o servidor
-                fetch('../desenvolvimento/backend/acoes_depoimentos.php', {
+                fetch('../desenvolvimento/backend/acoes_feedback.php', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded',
@@ -104,11 +104,11 @@
                     })
                     .then(response => response.text())
                     .then(data => {
-                        alert("Depoimento excluído com sucesso!");
+                        alert("Feedback excluído com sucesso!");
                         location.reload(); // Recarrega a página para atualizar a lista
                     })
                     .catch(error => {
-                        alert("Erro ao excluir o depoimento: " + error);
+                        alert("Erro ao excluir o feedback: " + error);
                     });
             }
         }
