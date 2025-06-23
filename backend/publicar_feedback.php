bceae05f0c8cd2ca62983541b8f8398ceb8c8f9f
CrislainyMartins <crislainy6339@gmail.com>
2025-06-23 08:52:08 -0300

    v4

--- backend/publicar_feedback.php
+++ backend/publicar_feedback.php
@@ -2,19 +2,22 @@
 require_once 'conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nome = $_POST['nome'] ?? 'Anônimo'; // Nome opcional
-    $avaliacao = $_POST['avaliacao'] ?? ''; //variavel da coluna avaliacao
-    $sugestao = $_POST['sugestao'] ?? ''; //variavel da coluna sugestao
+    $nome = $_POST['nome'] ?? 'Anônimo';
+    $avaliacao = $_POST['avaliacao'] ?? '';
+    $sugestao = $_POST['sugestao'] ?? '';
 
-    // Validar a mensagem
     if (trim($sugestao) === '') {
         die("A mensagem não pode estar vazia.");
     }
 
-    // Inserir no banco de dados
-    $sql = "INSERT INTO feedbacks (	nome, avaliacao, sugestao) VALUES (?, ?)";
+    $sql = "INSERT INTO feedbacks (nome, avaliacao, sugestao) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sis", $nome, $avaliacao,  $sugestao);
+
+    if (!$stmt) {
+        die("Erro ao preparar a query: " . $conn->error);
+    }
+
+    $stmt->bind_param("sis", $nome, $avaliacao, $sugestao);
 
     if ($stmt->execute()) {
         echo "Feedback enviado com sucesso!";
