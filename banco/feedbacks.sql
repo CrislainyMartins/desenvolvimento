427de149383674e541f89e863b64f981c1f1dad1
CrislainyMartins <crislainy6339@gmail.com>
2025-06-25 07:40:17 -0300

    v1

--- banco/feedbacks.sql
+++ banco/feedbacks.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23/06/2025 às 13:34
+-- Tempo de geração: 25-Jun-2025 às 12:38
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.0.30
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,35 +24,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `feedbacks`
+-- Estrutura da tabela `feedbacks`
 --
 
 CREATE TABLE `feedbacks` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `avaliacao` tinyint(3) UNSIGNED NOT NULL,
-  `sugestao` text NOT NULL
+  `sugestao` text NOT NULL,
+  `data_envio` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`id`, `nome`, `avaliacao`, `sugestao`, `data_envio`) VALUES
+(1, 'Maria ', 10, 'Em nada', '2025-06-23 18:51:33'),
+(2, 'Ana', 10, 'Para mim está excelente', '2025-06-23 18:58:26');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `feedbacks`
+-- Índices para tabela `feedbacks`
 --
 ALTER TABLE `feedbacks`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `feedbacks`
 --
 ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
